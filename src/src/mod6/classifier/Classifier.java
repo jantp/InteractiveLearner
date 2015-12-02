063728c6a7dc50b0cb5f19fb67f3c765ae291aab
Tycho <t.teesselink@student.utwente.nl>
2015-12-02 13:20:20 +0100

    Counter file

--- src/src/mod6/classifier/Classifier.java
+++ src/src/mod6/classifier/Classifier.java
@@ -1,13 +1,17 @@
 package mod6.classifier;
 
+import java.util.HashMap;
+
 /**
  * Created by Jan on 12/2/2015.
  */
 public class Classifier {
+
+    private HashMap<Integer, String> content;
     /**
      * The constructor of the class used to classify documents
      */
-    public Classifier () {
-
+    public Classifier (HashMap<Integer, String> in) {
+        this.content = in;
     }
 }
