0e9834476f5a6f36956324dc627b9b89135c70ef
Jan Thiemen <jan.thiemen.postema@gmail.com>
2015-12-02 11:40:59 +0100

    Create Classiefier class

---
+++ src/src/mod6/classifier/Classifier.java
@@ -0,0 +1,13 @@
+package mod6.classifier;
+
+/**
+ * Created by Jan on 12/2/2015.
+ */
+public class Classifier {
+    /**
+     * The constructor of the class used to classify documents
+     */
+    public Classifier () {
+
+    }
+}
