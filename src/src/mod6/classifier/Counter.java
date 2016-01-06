429725e9859db58b3445afbb45bdca7de9333f7f
Tycho <t.teesselink@student.utwente.nl>
2016-01-06 11:51:06 +0100

    browse functie toegevoegd

--- src/src/mod6/classifier/Counter.java
+++ src/src/mod6/classifier/Counter.java
@@ -1,9 +1,7 @@
 package mod6.classifier;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by Tycho on 12/2/15.
@@ -13,6 +11,7 @@ public class Counter {
     private LinkedList<String> documentWords;
     private HashMap<String, Integer> indexes;
     private String words;
+    private Set<String> stopwords = new HashSet<>();
 
     public Counter(LinkedList<String> in) {
         this.documentWords = in;
@@ -22,17 +21,39 @@ public class Counter {
         indexes = new HashMap<String, Integer>();
         int count = 0;
         LinkedList<String> frontier = new LinkedList<String>();
+        this.loadStopwords(new File("/Users/Tycho/Desktop/stopwords.txt"));
         while(this.documentWords.size() > 0) {
             String words =  this.documentWords.getFirst();
-            if (!(frontier.contains(words))) {
+            if (!(frontier.contains(words)) && !stopwords.contains(words)) {
                 count = Collections.frequency(this.documentWords, words);
                 indexes.put(words, count);
                 frontier.add(words);
                 this.documentWords.removeAll(Collections.singleton(words));
+            } else if (stopwords.contains(words)) {
+                this.documentWords.removeAll(Collections.singleton(words));
+                frontier.add(words);
             }
         }
-        //System.out.println(this.indexes.toString());
         return this.indexes;
+    }
+
+    public String loadStopwords(File trainingFile) {
+        try {
+            FileReader fr = new FileReader(trainingFile);
+            BufferedReader br = new BufferedReader(fr);
+            String document = "";
+            String line;
+            while ((line = br.readLine()) != null) {
+                stopwords.add(line);
+            }
+            return document;
+        } catch(FileNotFoundException e) {
+            System.err.println("File not found: "+trainingFile.getAbsolutePath());
+            return "";
+        } catch (IOException e) {
+            System.err.println("IO error on file: "+trainingFile.getAbsolutePath());
+            return "";
+        }
     }
 
 }
